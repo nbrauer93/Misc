31ee40ceb42fa682e528373f0c57dc7eda266d22
Noah Brauer <noah.brauer@colorado.edu>
2021-11-04 13:01:28 -0500

    Update Stage_4.py

--- Stage_4.py
+++ Stage_4.py
@@ -13,13 +13,6 @@ from netCDF4 import Dataset, num2date, MFDataset
 import netCDF4 as nc
 
 
-import os
-import conda
-
-conda_file_dir = conda.__file__
-conda_dir = conda_file_dir.split('lib')[0]
-proj_lib = os.path.join(os.path.join(conda_dir, 'share'), 'proj')
-os.environ["PROJ_LIB"] = proj_lib
 from mpl_toolkits.basemap import Basemap
 import pyart
 
