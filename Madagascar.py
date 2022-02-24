b8cd33de9d25bf40cde986563fe8fc9241845a1b
Noah Brauer <noah.brauer@colorado.edu>
2022-02-23 19:15:50 -0600

    Add files via upload

---
+++ Madagascar.py
@@ -0,0 +1,398 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 23 17:10:59 2022
+
+@author: noahbrauer
+"""
+
+
+import h5py 
+import numpy as np
+import matplotlib.pyplot as plt
+from pyproj import Proj
+from mpl_toolkits.basemap import Basemap
+
+
+file = '2A.GPM.DPR.V9-20211125.20220222-S002431-E015703.045368.V07A.HDF5'
+
+
+#Read in file, extract lat, lons, PSD
+
+DPR = h5py.File(file, 'r')
+
+#nscan is the nummber of scans in each granule 
+#nray is the number of angle bins in each  scan; think of these as footprint scans (5 km in diameter for each footprint)
+#nbin is the number of range bins in each ray (angle bins)
+#nDSD: Parameters are log_{10}(N_{w}) (number concentration) and D0 (mean drop diameter)
+
+
+lat = DPR['FS']['Latitude'][:,:]    
+lon = DPR['FS']['Longitude'][:,:]
+z = DPR['FS']['SLV']['zFactorFinalESurface'][:,:,0] #nscan x nray (7934,49)
+dsd = DPR['FS']['SLV']['paramDSD'][:] 
+freezing = DPR['FS']['VER']['heightZeroDeg'][:]
+
+#%%
+
+###Define cross-section length
+
+ind1 = np.where((lon[:,0]>=49)) #Where are the DPR lons >= -100
+ind2 = np.where((lon[:,0])<=54) #Where are lons <= -85
+ind3 = np.intersect1d(ind1,ind2) #Both conditions need to be satisfied here
+
+
+#Change the ray to change cross-section location (i.e. the "27" in this case)
+
+
+###Setup to 2D grid for plotting
+
+x = 2.* 17 #48 degrees (from -17 to 17)
+re = 6378. #radius of the earth
+theta = -1*(x/2.) + (x/48.)*np.arange(0,49) #Split into equal degrees (from -17 to 17)
+theta2  = np.ones(theta.shape[0]+1)*np.nan #Define an empty array (NaNs) with same shape as ray dimension
+theta = theta - 0.70833333/2. #Shift degrees for plotting pruposes
+theta2[:-1] = theta #remove last array dimension in theta so python won't get angry about shape
+theta2[-1] = theta[-1] + 0.70833333
+theta = theta2*(np.pi/180.) #Convert from degrees to radians
+
+prh = np.ones((177,50))*np.nan #Define empty grid
+
+for i in range(prh.shape[0]): #Loop over each range gate
+    for j in range(prh.shape[1]): #Loop over each scan
+            a = np.arcsin(((re+407)/re)*np.sin(theta[j]))-theta[j] #Orbit height of 407 km 
+            
+            prh[i,j] = (176-(i))*0.125*np.cos(theta[j]+a)
+            
+h2 = prh #where prh is the (range bins,ray)-space
+h3 =np.ones((177,50))*np.nan
+
+for i in range(h3.shape[1]):
+    h3[:,i] = h2[::-1,i] #This reverses the vertical dimension so as indices increase, height increases
+
+
+ku = DPR['FS']['SLV']['zFactorFinal'][ind3,:,:] #Read in ku-band reflectivity; nscan x nray (554,49,176)
+n0 = dsd[ind3,:,:,0]/10 #Read in the number concentration
+d0 = dsd[ind3,:,:,1] #Read in the mean drop diameter  #Both have dimensions nscan x nray x nbin (554,49,176)
+zeroDeg = freezing[ind3,:]
+
+#Cut all parameters so they are at same ray as above
+#45 is default
+
+ray = 3
+
+ku = ku[:,ray,:]
+n0 = n0[:,ray,:]
+d0 = d0[:,ray,:]
+zero_deg_isotherm = zeroDeg[:,ray]/1000 #Convert from meters to km
+
+#Take lats and lons along same ray
+lons = DPR['FS']['Longitude'][ind3,ray]
+lats = DPR['FS']['Latitude'][ind3,ray]
+
+
+
+
+#Choose a starting point, then calculate distance
+lat0 = lats[0]
+lon0 = lons[0]
+
+
+p = Proj(proj='laea', zone=10, ellps='WGS84',lat_0=lat0,lon_0=lon0) #Define a projection and set starting lat an lon to same point as above
+
+
+#%%
+
+#Define a 2D array for plotting purposes
+
+lat_3d = np.ones(ku.shape)*np.nan
+lon_3d = np.ones(ku.shape)*np.nan
+
+for i in range(ku.shape[0]):
+    lat_3d[i,:] = lats[i] 
+    lon_3d[i,:] = lons[i]  
+        
+
+x,y = p(lon_3d,lat_3d) #Now convert degrees to distance (in km)
+R_gpm = np.sqrt(x**2 + y**2)*np.sign(x) #Keeps sign of number; converts to radial distance 
+
+#Reverse range gate order for all parameters
+
+ku = ku[:,::-1,0]
+n0 = n0[:,::-1]
+d0 = d0[:,::-1]
+
+
+
+ku = np.ma.masked_where(ku<=12, ku) #Mask all the bad points in ku data
+y = np.ones([ku.shape[0], ku.shape[1]]) #Define an empty array
+
+#Define the appropriate range bins
+h4 = h3[:,ray] #This number MUST match the same ray being used
+for i in range(y.shape[1]):
+    y[:,i] = h4[i]
+    
+#%%
+
+
+
+#Determine median value of ind3 (intersection between longitudes)
+cross_track_index = int(len(ind3)/2)+ind3[0]
+
+
+
+#Let's plot a map with the center point location for our cross-sections
+
+#Mask near surface reflectivity values
+
+z = np.ma.masked_where(z<=12, z)
+
+
+
+cmin = 12.; cmax = 70.; cint = 2.5; clevs = np.round(np.arange(cmin,cmax,cint),2)
+nlevs = len(clevs) - 1; cmap = plt.get_cmap(name='turbo',lut=nlevs)
+    
+plt.figure(figsize=(10,10))
+  
+#xlim = np.array([-110,-75]); ylim = np.array([15,40])
+xlim = np.array([45.5,53]); ylim = np.array([-24,-15])
+#xlim = np.array([35,55]); ylim = np.array([-30,-10])
+ 
+
+  
+m = Basemap(projection='cyl',lon_0=np.mean(xlim),lat_0=np.mean(ylim),llcrnrlat=ylim[0],urcrnrlat=ylim[1],llcrnrlon=xlim[0],urcrnrlon=xlim[1],resolution='i')
+m.drawstates(), m.drawcountries(), m.drawcoastlines()
+cs = m.contourf(lon,lat,z,clevs,cmap='turbo',extend='both')
+cs2 = m.plot(lon[:,0]+0.03,lat[:,0]-0.03,'--k',zorder=4)
+cs3 = m.plot(lon[:,-1]-0.03,lat[:,-1]+0.03,'--k',zorder=4)
+m.plot(lon[ind3,ray],lat[ind3,ray],'-w',zorder=4,lw=0.25,alpha=0.75)
+m.plot(lons[0:160],lats[0:160], 'k--', linewidth = 4)
+
+
+##Change this to modify star size (cross-section starting point)
+m.plot(lon[ind3[0],ray],lat[ind3[0],ray],'*w',markersize = 30, zorder=4,lw=0.25,alpha=0.75,markerfacecolor='k',markeredgewidth=0.25)
+print(lon[ind3[0],ray])
+print(lat[ind3[0],ray])
+
+
+m.plot(lon[cross_track_index,:],lat[cross_track_index,:],'-w',zorder=4,lw=0.25,alpha=0.75)
+m.plot(lon[cross_track_index,0],lat[cross_track_index,0],'*w', zorder=4,lw=0.25,alpha=0.75,markerfacecolor='k',markeredgewidth=0.25)
+m.drawcounties()
+parallels = np.arange(-60,60,step = 2)
+m.drawparallels(parallels, labels = [True, False, False, False])
+
+meridians = np.arange(0,360, step = 2)
+m.drawmeridians(meridians, labels = [False, False, False, True])
+cbar = m.colorbar(cs,size='2%')
+cbar.ax.set_ylabel('[dBZ]',name='Calibri',size=18)
+cticks = []
+for i in clevs:
+    cticks.append(int(i)) if i.is_integer() else cticks.append(i)
+    cbar.set_ticks(clevs[::4])
+    cbar.set_ticklabels(cticks[::4])
+for i in cbar.ax.yaxis.get_ticklabels():
+    i.set_family('Calibri')
+    i.set_size(20)
+   
+plt.title('GPM Overpass 02/22 0042 UTC KuPR', size = 20)    
+
+
+
+
+
+#Remove the values less than or equal to zero
+
+n0[n0<=0] = np.nan
+d0[d0<=0] = np.nan
+
+    
+    
+#%% 
+    
+#Now we plot! #N-S (along track first)  
+
+
+#Plot mean drop size first
+
+dist = np.array([0,900])
+
+x_loc = 500
+y_loc = 14.5
+label = '@NOAABrauer'
+
+plt.figure(figsize=(10,10))
+
+vmax = 3
+vmin = 0
+
+R_min = R_gpm.min()
+R_max = R_gpm.max()
+
+R_gpm = R_gpm[:,:,0]
+
+label_size = 20
+
+cmin =0.; cmax = 3.; cint = 0.1; clevs = np.round(np.arange(cmin,cmax,cint),2)
+nlevs = len(clevs) - 1; cmap = plt.get_cmap(name='turbo',lut=nlevs)
+pm = plt.pcolormesh(R_gpm/1000., y, d0, cmap='turbo',vmin=vmin,vmax=vmax)
+pm2 = plt.plot(R_gpm/1000., zero_deg_isotherm, '--', color = 'k')
+plt.xlabel('Along Track Distance (km)', size = 20)
+plt.ylabel('Altitude (km)', size = 20)
+plt.title(r'GPM Overpass 02/22 0042 UTC $D_{M}$ ', size = 20)
+plt.xlim(dist[0],dist[1])
+plt.ylim(0,12)
+cbar = plt.colorbar()
+cbar.ax.tick_params(labelsize = label_size)
+cbar.set_label(label = '[mm]',size = label_size)
+plt.xticks(size = label_size)
+plt.yticks(size = label_size)
+
+plt.clim(0,3)
+
+
+#plt.text(x_loc, y_loc, label)
+
+
+plt.show()    
+    
+
+
+####Number concentration (liquid water content)
+
+plt.figure(figsize=(10,10))
+
+vmax = 6
+vmin = 1
+
+R_min = R_gpm.min()
+R_max = R_gpm.max()
+
+
+
+cmin =1.; cmax = 6.; cint = 0.25; clevs = np.round(np.arange(cmin,cmax,cint),2)
+nlevs = len(clevs) - 1; cmap = plt.get_cmap(name='turbo',lut=nlevs)
+pm = plt.pcolormesh(R_gpm/1000., y, n0, cmap='turbo',vmin=vmin,vmax=vmax)
+pm2 = plt.plot(R_gpm/1000., zero_deg_isotherm, '--', color = 'k')
+plt.xlabel('Along Track Distance (km)', size = 20)
+plt.ylabel('Altitude (km)', size = 20)
+plt.title(r'GPM Overpass 02/22 0042 UTC $log_{10 }(N_{W})$ ', size = 20)
+plt.xlim(dist[0],dist[1])
+plt.ylim(0,12)
+cbar = plt.colorbar()
+cbar.ax.tick_params(labelsize = label_size)
+cbar.set_label(label = r'[$m^{-3}$ $mm^{-1}$]',size = label_size)
+plt.clim(1,6)
+#plt.text(x_loc, y_loc, label)
+plt.xticks(size = label_size)
+plt.yticks(size = label_size)
+
+plt.show()    
+    
+
+###And lastly Ku-band
+
+
+plt.figure(figsize=(10,10))
+
+vmax = 60
+vmin =12
+
+R_min = R_gpm.min()
+R_max = R_gpm.max()
+
+
+
+cmin =12.; cmax = 60.; cint = 2.5; clevs = np.round(np.arange(cmin,cmax,cint),2)
+nlevs = len(clevs) - 1; cmap = plt.get_cmap(name='turbo',lut=nlevs)
+pm = plt.pcolormesh(R_gpm/1000., y, ku, cmap='turbo',vmin=vmin,vmax=vmax)
+pm2 = plt.plot(R_gpm/1000., zero_deg_isotherm, '--', color = 'k')
+plt.xlabel('Along Track Distance (km)', size = 20)
+plt.ylabel('Altitude (km)', size = 20)
+plt.title(r'GPM Overpass 02/22 0042 UTC KuPR ', size = 20)
+#plt.xlim(300,450)
+plt.xlim(dist[0], dist[1])
+plt.ylim(0,12)
+cbar = plt.colorbar()
+cbar.ax.tick_params(labelsize = label_size)
+cbar.set_label(label = '[dBZ]',size = label_size)
+plt.clim(12,60)
+plt.xticks(size = label_size)
+plt.yticks(size = label_size)
+
+#plt.text(x_loc, y_loc, label)
+plt.show()   
+
+#%%
+
+
+
+
+fig,ax = plt.subplots(figsize=(14,14))
+ku[ku<0] = np.nan
+
+ku_25 = np.nanpercentile(ku,25,axis = 0)
+ku_50 = np.nanpercentile(ku,50,axis = 0)
+ku_75 = np.nanpercentile(ku,75,axis = 0)
+ku_95 = np.nanpercentile(ku,95,axis = 0)
+
+
+
+x_bins = np.arange(10,60,1)
+y_bins = np.arange(0,21.988032,0.124932)
+
+
+
+fig,ax = plt.subplots(figsize=(14,14)) 
+
+xtick_label_size = 20
+ytick_label_size = 20
+tick_label_size = 26
+title_size = 28
+
+
+
+
+
+
+xlabels = np.arange(10,65,5)
+#ylabels = np.arange(0,13.5,0.5)
+ylabels = np.arange(-6,6,0.5)
+
+plt.xticks(xlabels)
+plt.yticks(ylabels)
+
+
+plt.xlim(10,55)
+plt.ylim(-6,6)
+
+
+plt.ylabel('Normalized Altitude (km)', size = 26)
+plt.xlabel('dBZ', size = 26)
+
+ax.set_xticklabels(['10','15','20', '25', '30', '35', '40', '45', '50','55','60'], size = xtick_label_size)
+plt.yticks(size =ytick_label_size )        
+plt.title('02/22 0042 UTC Along-Track KuPR Percentiles', size = title_size)
+
+
+zero_deg_mean = np.nanmean(zero_deg_isotherm)
+
+#Normalize altitude
+
+norm_altitude = np.ones((y_bins.shape))*np.nan
+
+for i in range(len(y_bins)):
+    
+    norm_altitude[i] = y_bins[i] - zero_deg_mean
+    
+    
+
+#altitude_norm = 
+
+plt.plot(ku_25,norm_altitude, label = '25p', color = 'k')
+plt.plot(ku_50,norm_altitude,label = '50p', color = 'r')
+plt.plot(ku_75,norm_altitude,label = '75p', color = 'b')
+plt.plot(ku_95,norm_altitude,label = '95p', color = 'g')
+plt.axhline(y=0,color = 'k', linestyle = '--')
+plt.legend(fontsize = 'xx-large')
+
+plt.show()
