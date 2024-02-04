b251d93f7789afabb09497882e201571d06df5b6
Noah Brauer <noah.brauer@noaa.gov>
2024-02-04 15:54:17 -0700

    Add files via upload

---
+++ Denver_20240203_QVP.py
@@ -0,0 +1,167 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 23 12:08:46 2023
+
+@author: noahbrauer
+"""
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pyart
+import cartopy.crs as ccrs
+import glob
+#from tqdm import tqdm
+from scipy import stats
+
+radar_file = 'KFTG20240203_233901_V06'
+
+
+#import numpy as np
+
+#from pyart.io import read
+#from pyart.core import antenna_to_cartesian
+
+
+
+def quasi_vertical_profile(filename, fields=None, gatefilter=None):
+
+     radar = pyart.io.read(filename)
+
+     if fields is None:
+         fields = radar.fields
+
+    #if gatefilter is not None:
+
+
+     desired_angle = 20.0
+     index = abs(radar.fixed_angle['data'] - desired_angle).argmin()
+     #print(radar.fixed_angle['data'])
+     #print(radar.elevation['data'][-1])
+
+     qvp = {}
+
+     for field in fields:
+         this_field = radar.get_field(index, field).mean(axis = 0)
+         qvp.update({field:this_field})
+
+     qvp.update({'range': radar.range['data'], 'time': radar.time})
+     x,y,z = pyart.core.antenna_to_cartesian(qvp['range']/1000.0, 0.0,
+                                 radar.fixed_angle['data'][index])
+     qvp.update({'height': z})
+     #del radar
+     return qvp
+
+qvp = quasi_vertical_profile(radar_file)
+#print(qvp['reflectivity'])
+
+plt.plot(qvp['differential_reflectivity'],qvp['height']/1000.0)
+plt.xlabel('Mean Differential Reflectivity (dB)')
+plt.ylabel('Altitude')
+plt.title('Quasi-Vertical Profile 2154 UTC')
+
+#plt.show()
+
+
+#Now do this for multiple files
+
+
+
+files = glob.glob('KFTG*')
+
+#Loop through file list to get times; Parse out of file radar_name
+#Sort file names by time first:
+
+order_index = np.argsort(files)
+files_ordered = [files[i] for i in order_index]
+
+times = []
+qvps_z = np.ones((135,1832))*np.nan
+qvps_zdr = np.ones((135,1832))*np.nan
+qvps_rhohv = np.ones((135,1832))*np.nan
+
+for i in range(len(files_ordered)):
+
+    qvps = quasi_vertical_profile(files_ordered[i])
+
+    qvps_z[i,:] = qvps['reflectivity']
+    qvps_zdr[i,:] = qvps['differential_reflectivity']
+    qvps_rhohv[i,:] = qvps['cross_correlation_ratio']
+
+    parsed_time = files_ordered[i].split("_")[1]
+    print(parsed_time)
+    times.append(parsed_time)
+
+#print(qvps_z.shape) # 107 x 1832
+#print(np.nanmax(qvps_z))  # 44.495 dBZ
+
+times = np.asarray(times)
+
+
+
+#%%
+
+
+#Now plot:
+
+font_size = 16
+title_size = 20
+
+plt.figure(figsize=(10,10))
+
+qvps_z[qvps_rhohv<0.7] = np.nan
+qvps_zdr[qvps_rhohv<0.7] = np.nan
+qvps_rhohv[qvps_rhohv<0.7] = np.nan
+
+plt.pcolormesh(times,qvp['height']/1000.0,qvps_z.T, cmap = 'turbo')
+plt.xlabel('Time (UTC)', size= font_size)
+plt.ylabel('Height (km)', size = font_size)
+cbar = plt.colorbar()
+cbar.ax.tick_params(labelsize = font_size)
+cbar.set_label(label = '[dBZ]',size = font_size)
+#plt.colorbar()
+plt.clim(-15,30)
+plt.ylim(0,10)
+
+plt.title(r'KFTG 02/03 $Z_{H}$', size = title_size)
+x = [0,27,54,81,108,135]
+labels = np.array(['1200','1400','1600','1800','2000','2200','2400'])
+plt.xticks(x,labels,size = font_size)
+plt.yticks(size = font_size)
+plt.savefig('Z_qvp.png', dpi = 300)
+plt.show()
+
+
+plt.figure(figsize=(10,10))
+
+plt.pcolormesh(times,qvp['height']/1000.0,qvps_zdr.T, cmap = 'pyart_RefDiff')
+plt.xlabel('Time (UTC)', size = font_size)
+plt.ylabel('Height (km)', size = font_size)
+plt.ylim(0,10)
+plt.clim(-0.5,3)
+cbar = plt.colorbar()
+cbar.ax.tick_params(labelsize = font_size)
+cbar.set_label(label = '[dB]',size = font_size)
+plt.title(r'KFTG 02/03 $Z_{DR}$', size = title_size)
+
+plt.xticks(x,labels,size = font_size)
+plt.yticks(size = font_size)
+plt.savefig('zdr_qvp.png', dpi = 300)
+plt.show()
+
+plt.figure(figsize=(10,10))
+
+plt.pcolormesh(times,qvp['height']/1000.0,qvps_rhohv.T, cmap = 'turbo')
+plt.xlabel('Time (UTC)', size = font_size)
+plt.ylabel('Height (km)', size = font_size)
+plt.ylim(0,10)
+plt.clim(0.9,1.)
+cbar = plt.colorbar()
+cbar.ax.tick_params(labelsize = font_size)
+cbar.set_label(label = r'[$\rho_{HV}$]',size = font_size)
+plt.title(r'KFTG 02/03 $\rho_{HV}$', size = title_size)
+
+plt.xticks(x,labels,size = font_size)
+plt.yticks(size = font_size)
+plt.savefig('rhohv_qvp.png', dpi = 300)
+plt.show()
---
+++ Denver_Plot_Snow_20240203.py
@@ -0,0 +1,106 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from mpl_toolkits.basemap import Basemap
+from netCDF4 import Dataset, num2date, MFDataset
+import netCDF4 as nc
+import glob
+import imageio
+import os
+
+file_z = 'KFTG_V06_20240203_220205.nc'
+
+
+
+
+nc = Dataset(file_z, 'r')
+
+latitude = nc.variables['lat'][:]
+longitude = nc.variables['lon'][:]
+
+lat2,lon2 = np.meshgrid(latitude, longitude)
+
+
+files = glob.glob('*.nc')
+
+#Let's parse the end of the file name, then sort to grab indices
+
+times_parsed = []
+#times_string = []
+
+for i in range(len(files)):
+
+    first_split = files[i].split("_")[3]
+    second_split = first_split.split(".")[0]
+    third_split = second_split[0:4]
+
+    times_parsed.append(third_split)
+
+    #Remove first zero from string:
+
+
+    #third_split = second_split[1:]
+    #times_parsed.append(third_split)
+
+    #times_used = third_split[0:3]
+    #times_string.append(times_used)
+
+
+#Now sort times
+
+times_ordered_index = np.argsort(times_parsed)
+
+
+times_ordered = [files[i] for i in times_ordered_index]
+
+
+
+
+#%%
+
+
+#order time
+
+time_array = np.array(times_parsed)
+time_array_sort = [time_array[i] for i in times_ordered_index]
+
+print(time_array_sort)
+
+
+for file in range(len(times_ordered)):
+
+    data = Dataset(times_ordered[file], 'r')
+    print(times_ordered[file])
+
+    zh = data.variables['Reflectivity'][:]
+
+    zh[zh<0] = np.nan
+
+    #Now plot
+    plt.figure(figsize=(20,20))
+
+    cmin = 0; cmax = 60; cint = 2; clevs = np.round(np.arange(cmin,cmax,cint),2)
+    xlim = np.array([-106,-103.]); ylim = np.array([38,41])
+
+    m = Basemap(projection='cyl',lon_0=np.mean(xlim),lat_0=np.mean(ylim),llcrnrlat=ylim[0],urcrnrlat=ylim[1],llcrnrlon=xlim[0],urcrnrlon=xlim[1],resolution='i')
+    m.drawstates(); m.drawcountries()
+    m.drawcounties()
+    cs = m.contourf(lon2,lat2,zh[0,:,:].T, clevs, cmap = 'turbo', extend = 'both')
+
+    x_loc = -103.75
+    y_loc = 38.15
+    label = '@NOAABrauer'
+    plt.text(x_loc, y_loc, label, size = 24)
+
+    x = -104.99
+    y = 39.74
+
+    m.plot(x,y,'or', markerfacecolor= 'w', markersize = 14)
+
+
+    cbar = plt.colorbar()
+    cbar.ax.tick_params(labelsize = 26)
+    cbar.set_label(label = '[dBZ]',size = 26)
+
+    plt.title(r'KFTG 02/03 ' + time_array_sort[file] + ' UTC' + ' $0.5^{o} Z_{H}$', size = 40)
+    plt.savefig(time_array_sort[file] + ".png", dpi = 300)
+    #plt.show()
